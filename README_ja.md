e75c0bae38ea87d4eb95ad130ed74fb9d7979e81
GNagahashi <88316240+GNagahashi@users.noreply.github.com>
2022-11-23 18:40:02 +0900

    add setup_*.sh

--- README_ja.md
+++ README_ja.md
@@ -1,1 +1,19 @@
 # enpit サマースクール 2022
+前提条件
+- vmwareのインストールが完了している
+- 仮想環境のダウンロードが完了している
+- ダウンロードした仮想環境をvmwareで開けている(いつでも操作できる状態)
+
+## 1. セットアップ
+はじめに、このリポジトリをcloneしてください(重いファイルがあるので、少し時間が掛かります)
+```sh
+git clone https://github.com/GNagahashi/drone-claw-machine.git drone_claw_machine
+```
+
+次に、cloneしてきたリポジトリ内にあるセットアップ用のシェルスクリプトを順に実行していきます
+```sh
+cd ~/drone_claw_machine
+sh setup_01.sh
+sh setup_02.sh
+```
+シェルスクリプトが正しく実行されない場合、シェルスクリプトに書かれたコマンドを1行ずつ手動で行ってください
---
+++ setup_01.sh
@@ -0,0 +1,15 @@
+cd ~/drone_claw_machine/model_drone
+sudo cp /usr/share/gazebo-9/models/iris_with_standoffs/model.sdf ./model.sdf.bak
+sudo cp model.sdf /usr/share/gazebo-9/models/iris_with_standoffs/
+
+cd ~/drone_claw_machine/model_gzobject
+cp -r * ~/.gazebo/models
+
+
+cd ~/catkin_ws/src
+git clone https://github.com/GNagahashi/enpit2022-summer.git gnc
+git clone https://github.com/GNagahashi/enpit2022-summer-drone-ctrl.git drone_ctrl
+git clone https://github.com/GNagahashi/enpit2022-summer-drone-position.git drone_position
+
+cd ~/catkin_ws
+catkin build
